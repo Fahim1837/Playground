f366f0b1b6c398c5f544fd60e23a474a9c0d35c9
Fahim Ahmed <Fahim1837>
2024-01-24 17:41:00 +0600

    Create usePosts Hook

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,7 +1,17 @@
+import usePosts from "../hooks/usePosts"
 
 function PostList() {
-  return (
-    <div>PostList</div>
+    
+
+    const {data: posts, error, isLoading} = usePosts()
+    return (
+        <>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{`${error.name}: ${error.message}`}</p>}
+    <ul className="list-group mt-4">
+        {posts?.map((item) => <li className="list-group-item" key={item.id}>{item.title}</li>)}
+    </ul>
+    </>
   )
 }
 
---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
+interface Posts {
+    userID: number
+    id: number
+    title: string
+    body: string
+}
+
+const usePosts = () => {
+    const fetchPosts = () => 
+    axios.get <Posts[]> ('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => res.data)
+
+    return useQuery({
+        queryKey: ['posts'],
+        queryFn: fetchPosts
+    })
+}
+
+export default usePosts
